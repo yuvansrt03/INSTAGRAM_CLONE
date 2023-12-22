eed16fe964a3eee903e47929750b337aefd01c8a
Yuvan <yuviyuvan15@gmail.com>
2023-12-22 21:09:04 +0530

    onClick Login

--- client/src/Components/LoginPage/LoginPage.js
+++ client/src/Components/LoginPage/LoginPage.js
@@ -79,6 +79,7 @@ function LoginPage() {
           <button
             className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
             type="submit"
+            onClick={handleSubmit}
           >
             Sign In
           </button>
